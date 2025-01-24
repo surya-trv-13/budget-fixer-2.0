2d026dd4d4c8d84450edb14affba94c606217bbc
Suryanarayan Rath <suryanarayanrath74@gmail.com>
2025-01-24 17:30:05 +0530

    Re addition of Redux

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,29 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { inter } from "@/app/ui/fonts"
+import { inter } from "@/app/ui/fonts";
 import { Provider } from "./provider";
+import StoreProvider from "./StoreProvider";
 
 export const metadata: Metadata = {
-  title: {
-    template: '%s | Budget Fixer 2.0',
-    default: 'Budget Fixer 2.0',
-  },
-  description: 'The Official Budget Management for salaried employee.',
+	title: {
+		template: "%s | Budget Fixer 2.0",
+		default: "Budget Fixer 2.0",
+	},
+	description: "The Official Budget Management for salaried employee.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-        <Provider>
-          <body
-            className={`${inter.className} antialiased`}
-          >
-            {children}
-          </body>
-        </Provider>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<StoreProvider>
+				<Provider>
+					<body className={`${inter.className} antialiased`}>{children}</body>
+				</Provider>
+			</StoreProvider>
+		</html>
+	);
 }
